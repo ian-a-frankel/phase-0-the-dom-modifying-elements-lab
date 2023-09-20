6b92b011a061154d475b85be2ca786413ba3895b
Ian Frankel <ian.frankel@gmail.com>
2023-09-20 16:32:16 -0400

    victory lap

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+document.querySelector("main").remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.textContent = "Ian is the champion";
+
+newHeader.id = 'victory';
+
+document.body.append(newHeader);
